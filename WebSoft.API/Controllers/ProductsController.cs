50669fca392566b2751a83486aa967ef96bf860a
Md. Zahidul Islam <zislamcse41@gmail.com>
2024-01-11 07:40:11 +0600

    Create Project

---
+++ WebSoft.API/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSoft.API.Controllers
+{
+    //https://localhost:7104/Products
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string[] products = new string[] { "Napa 500", "Napa 300", "Rupa", "Cef-3" };
+            return Ok(products);
+        }
+    }
+}
